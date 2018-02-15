241ebdd95c55286aba8b684cfe84aacfb2eb9894
jyjek <shpiruk@pup.local>
2018-02-15 11:58:36 +0200

    add complete history function

--- R/filtNA.R
+++ R/filtNA.R
@@ -4,18 +4,15 @@
 #'
 #' @param z0 dataset with historical sales
 #' @return data frame with clear data
-#' @import dplyr
+#' @importFrom dplyr %>%
 #' @export
 #'
 filtNA<- function(z0){
 
   d<-z0%>%
     dplyr::group_by(SKU)%>%
-    dplyr::mutate(#gift=if_else(is.na(gift),0,gift),
-      #main=if_else(is.na(main),0,main),
-      #present_1c=if_else(is.na(present_1c),0,present_1c),
-      #sales_num=ifelse((present_1c==1 | gift==1 | main==1 | aval %in% c("S","A","O","N","ZZ") ),
-      #                  NA,sales_num),
+    dplyr::mutate(
+      sales_num=ifelse(isAction==1,NA,sales_num),
       sales_num=ifelse(is.na(sales_num),mean(sales_num,na.rm=T),sales_num),
       sales_num=if_else(is.nan(sales_num),0,sales_num))%>%
     #dplyr::select(date,SKU,sales_num)%>%
---
+++ R/hist_compl.R
@@ -0,0 +1,30 @@
+#' Complete History
+#'
+#' This function make complete history when some data is dropped
+#'
+#' @param z0 dataset with historical sales
+#' @param type type of dropped rows
+#' @return data frame with full historical dataset
+#' @importFrom dplyr %>%
+#' @export
+
+hist_compl<-function(z0,type="zero"){
+  if(type=="zero"){
+    q<-z0%>%
+      tidyr::complete(date,SKU,   fill = list(`in`=0,isAction=0,sales_num=0))%>%
+      dplyr::group_by(SKU)%>%
+      dplyr::mutate(balance_num=dplyr::if_else(is.na(balance_num),lag(balance_num,order_by=date),balance_num),
+                    price=dplyr::if_else(is.na(price),round(mean(price,na.rm=T),2),price),
+                    balance_num=dplyr::if_else(is.na(balance_num),mean(balance_num,na.rm = T),balance_num))
+  }
+  if(type=="drop"){
+    q<-z0%>%
+      tidyr::complete(date,SKU,   fill = list(`in`=0,isAction=0))%>%
+      dplyr::group_by(SKU)%>%
+      dplyr::mutate(sales_num=dplyr::if_else(is.na(sales_num),round(mean(sales_num,na.rm = T),2),sales_num),
+                    price=dplyr::if_else(is.na(price),round(mean(price,na.rm=T),2),price),
+                    balance_num=dplyr::if_else(is.na(balance_num),mean(balance_num,na.rm = T),balance_num))
+  }
+ return(q)
+}
+
