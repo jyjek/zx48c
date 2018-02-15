46e7f35f3ffb8df7ae8ca554886ac614eacdeac8
jyjek <shpiruk@pup.local>
2018-02-15 10:14:59 +0200

    cahnge filtration to few types

--- R/filt.R
+++ R/filt.R
@@ -1,22 +1,49 @@
 #' Filter historical data
+#'
 #' This function filter historical data from upper values and replace that to mean
+#'
 #' @param z0 dataset with historical sales
+#' @param type type of filtration ("both","upper","lower")
 #' @return data frame with clear data
-#' @import dplyr
+#' @importFrom  dplyr %>%
 #' @importFrom stats median
 #' @importFrom stats sd
 #' @export
 
-#globalVariables(c("new","new_sales"))
 
-filt <- function(z0){
-  q0<-z0%>%
-    #dplyr::select(SKU,date,sales_num)%>%
-    dplyr::group_by(SKU)%>%
-    mutate(sd=sd(sales_num),
-           new=ifelse(sales_num>mean(sales_num)+1.5*sd(sales_num),NA,sales_num ),
-           new_sales=ifelse(is.na(new),mean(new,na.rm=T)+sd,new))%>%
-    dplyr::select(-sales_num,-sd,-new)%>%
-    dplyr::rename(sales_num=new_sales)
+filt <- function(z0,type="both"){
+  if(type=="upper"){
+    q0<-z0%>%
+      dplyr::select(SKU,date,sales_num)%>%
+      dplyr::group_by(SKU)%>%
+      dplyr::mutate(sd=sd(sales_num,na.rm=T),
+                    new=ifelse(sales_num>mean(sales_num,na.rm=T)+1.5*sd(sales_num,na.rm=T),NA,sales_num ),
+                    new_sales=ifelse(is.na(new),mean(new,na.rm=T)+sd,new))%>%
+      dplyr::select(-sales_num,-sd,-new)%>%
+      dplyr::rename(sales_num=new_sales)
+  }
+  if(type=="both"){
+    q0<-z0%>%
+      dplyr::select(SKU,date,sales_num,balance_num)%>%
+      dplyr::group_by(SKU)%>%
+      dplyr::mutate(mn=mean(sales_num,na.rm=T),
+                    sales_num=ifelse(balance_num<mn,NA,sales_num),
+                    new=ifelse(sales_num>mean(sales_num,na.rm=T)+1.5*sd(sales_num,na.rm=T),NA,sales_num),
+                    new_sales=ifelse(is.na(new),mean(new,na.rm=T)+sd(new,na.rm=T),new))%>%
+      dplyr::select(SKU,date,new_sales)%>%
+      dplyr::rename(sales_num=new_sales)
+  }
+  if(type=="lower"){
+    q0<-z0%>%
+      dplyr::select(SKU,date,sales_num,balance_num)%>%
+      dplyr::group_by(SKU)%>%
+      dplyr::mutate(new=ifelse(balance_num<mean(sales_num,na.rm = T),NA,sales_num),
+                    new_sales=ifelse(is.na(new),mean(new,na.rm=T)+sd(new,na.rm=T),new),
+                    new_sales=ifelse(is.nan(new_sales),0,new_sales))%>%
+      dplyr::select(SKU,date,new_sales)%>%
+      dplyr::rename(sales_num=new_sales)
+  }
+  q0<-q0%>%
+    dplyr::mutate(sales_num=ifelse(is.na(sales_num),0,sales_num))
   return(q0)
 }
