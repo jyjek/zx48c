45126ee904ee24de01d5e5b5f047157e095c8673
jyjek <shpiruk.d@gmail.com>
2018-05-24 12:30:26 +0300

    add messages

--- R/hist_compl.R
+++ R/hist_compl.R
@@ -13,7 +13,6 @@ hist_compl<-function(z0,type="zero"){
   if(!comp %in% c("zero","drop")){
     stop('Wrong "comp" value. It must be "zero" or "drop"')
   }
-
   war<-z0%>%dplyr::group_by(SKU)%>%
     dplyr::summarise(count=n_distinct(date))%>%
     dplyr::filter(count<56)%>%
