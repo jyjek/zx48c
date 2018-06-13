9a66832a35dc8a0bf00ad506a67bd0b3de3b5eee
jyjek <shpiruk.d@gmail.com>
2018-06-13 16:38:30 +0300

    fixed

--- R/forecast.R
+++ R/forecast.R
@@ -59,7 +59,7 @@ if(transf=="sku"){
   if(transf=="fills"){
    fcst<-data%>%dplyr::filter(date>max(date)-lubridate::days(42))%>%
       filtNA(type=transf)%>%filt(type=filt,type_f=transf)%>%forecast()%>%
-      dplyr::left_join( fills_koef(data0),by="SKU")%>%
+      dplyr::left_join( fills_koef(data),by="SKU")%>%
       dplyr::mutate(ALL=round(ALL*proc,4))%>%
       dplyr::select(-proc)%>%
       dplyr::left_join(cat,by="SKU")%>%
